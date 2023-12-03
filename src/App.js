05a7a2d499c6d0f6b8ade60fa9355b20f4a5d89f
um-zagaa <zagdsuren.bat-erdene@unimedia.mn>
2023-12-03 11:45:48 +0800

    change rails api to nestjs back

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
   const [systemSetting, setSystemSetting] = useState([]);
 
   useEffect(() => {
-    client.get('v1/system_settings/1')
+    // client.get('v1/system_settings/1')
+    client.get('system_settings')
     .then(systemSetting => {
       console.log(systemSetting);
       setSystemSetting(systemSetting);
@@ -28,11 +29,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Table: {systemSetting.data == null ? 'empty' : systemSetting.data.data.type}
+          {/* Table: {systemSetting.data == null ? 'empty' : systemSetting.data.data.type} */}
+          Table: {'System Settings'}
         </p>
-        <div>id: {systemSetting.data == null ? 'empty' : systemSetting.data.data.id}</div>
+        {/* <div>id: {systemSetting.data == null ? 'empty' : systemSetting.data.data.id}</div>
         <div>name: {systemSetting.data == null ? 'empty' : systemSetting.data.data.attributes.name}</div>
-        <div>value: {systemSetting.data == null ? 'empty' : systemSetting.data.data.attributes.value}</div>
+        <div>value: {systemSetting.data == null ? 'empty' : systemSetting.data.data.attributes.value}</div> */}
+        <div>id: {systemSetting.data == null ? 'empty' : systemSetting.data[0].id}</div>
+        <div>name: {systemSetting.data == null ? 'empty' : systemSetting.data[0].name}</div>
+        <div>value: {systemSetting.data == null ? 'empty' : systemSetting.data[0].value}</div>
       </header>
     </div>
   );
